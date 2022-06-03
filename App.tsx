62299fabbfe70652c8ecf15b3b2b18ee5456cb90
Leonardo Kovaliski <leonardokovaliski@hotmail.com>
2022-06-03 12:43:48 -0300

    Criação de Telas e configuração de Form

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import AppLoading from 'expo-app-loading';
 import { ThemeProvider } from 'styled-components';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 import {
   useFonts,
@@ -11,7 +12,8 @@ import {
 
 import theme from './src/global/styles/theme';
 
-import { Dashboard } from './src/screens/Dashboard';
+import { NavigationContainer } from '@react-navigation/native';
+import { AppRoutes } from './src/routes/app.routes';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -26,7 +28,9 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Dashboard/>
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
     </ThemeProvider>
   );
 }
--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,112 @@
 import React from 'react';
 
-import { Container, Title } from './styles';
+import { HighligthCard } from '../../components/HighligthCard'
+import { TransactionCard, TransactionCardProps } from '../../components/TransactionCard'
+
+import {
+    Container,
+    Header,
+    UserWrapper,
+    UserInfo,
+    Photo,
+    User,
+    UserGreeting,
+    UserName,
+    Icon,
+    HighligthCards,
+    Transactions,
+    Title,
+    TransactionList
+} from './styles';
+
+export interface DataListProps extends TransactionCardProps {
+    id: string;
+}
 
 export function Dashboard (){
+    const data: DataListProps[] = [
+        {
+            id: '1',
+            type: 'positive',
+            title: 'Desenvolvimento de Site',
+            amount: 'R$ 12.000,00',
+            category: {
+                name: 'Vendas',
+                icon: 'dollar-sign'
+            },
+            date: '13/04/2020'
+        },
+        {
+            id: '2',
+            type: 'negative',
+            title: 'Hamburgueria Pizzy',
+            amount: 'R$ 59,00',
+            category: {
+                name: 'Alimentação',
+                icon: 'coffee'
+            },
+            date: '10/04/2020'
+        },
+        {
+            id: '3',
+            type: 'negative',
+            title: 'Aluguel do Apartamento',
+            amount: 'R$ 1.200,00',
+            category: {
+                name: 'Casa',
+                icon: 'shopping-bag'
+            },
+            date: '10/04/2020'
+        }
+    ];
     return (
         <Container>
-            <Title>Dashboard</Title>
+            <Header>
+                <UserWrapper>
+                    <UserInfo>
+                        <Photo 
+                            source={{ uri: 'https://avatars.githubusercontent.com/u/49030804?v=4' }}
+                        />
+                        <User>
+                            <UserGreeting>Olá,</UserGreeting>
+                            <UserName>Leonardo</UserName>
+                        </User>
+                    </UserInfo>
+
+                    <Icon name='power' />
+                </UserWrapper>
+            </Header>
+
+            <HighligthCards>
+                <HighligthCard
+                    type='up'
+                    title='Entradas' 
+                    amount='R$ 17.000,00' 
+                    lastTransaction='Última entrada dia 13 de Abril'
+                />
+                <HighligthCard
+                    type='down'
+                    title='Saídas' 
+                    amount='R$ 1.259,00' 
+                    lastTransaction='Última saída dia 03 de Abril'
+                />
+                <HighligthCard
+                    type='total'
+                    title='Total' 
+                    amount='R$ 16.141,00' 
+                    lastTransaction='01 à 16 de Abril'
+                />
+            </HighligthCards>
+
+            <Transactions>
+                <Title>Listagem</Title>
+
+                <TransactionList 
+                    data={data}
+                    keyExtractor={item => item.id}
+                    renderItem={({ item }) => <TransactionCard data={item} />}
+                />
+            </Transactions>
         </Container>
     )
 }
